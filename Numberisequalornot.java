ea708fcc52e29e65313edf7beb52ec2dd1c02883
Kundan kumar yadav <119559941+kkr8093@users.noreply.github.com>
2022-12-02 13:34:13 +0530

    Add files via upload

---
+++ Numberisequalornot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Numberisequalornot
+ {
+    public static void main(String[] args)
+    {
+        int firstNumber = 85;
+        int secondNumber = 15;
+        if ((firstNumber - secondNumber) == 0)
+            System.out.println("Numbers are equal");
+        else
+            System.out.println("Numbers are not equal");
+   }
+}
