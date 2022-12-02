080f9c238716e4ebf40cdfc0abca4af9a7f01f4a
Kundan kumar yadav <119559941+kkr8093@users.noreply.github.com>
2022-12-02 11:59:11 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+public class Even
+{
+public static void main(String args[])
+{
+int number = 52;
+if(number%2==0)
+{
+System.out.println("it is even number");
+}
+else
+{
+System.out.println("no is not even number");
+}
+}
+}
