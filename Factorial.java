eb5fbb4df054e4252241ae52fb54453c15ff93f0
Kundan kumar yadav <119559941+kkr8093@users.noreply.github.com>
2022-12-14 13:41:34 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package oops;
+class Fact
+{
+	public void factorial(int n)
+	{
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+	}
+public class AnonymousObjDemo {
+
+	public static void main(String[] args) {
+		new Fact().factorial(5);
+
+	}
+
+}
