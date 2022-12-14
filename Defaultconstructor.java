8bca8df1889f48606c927791d4580223885b7aa7
Kundan kumar yadav <119559941+kkr8093@users.noreply.github.com>
2022-12-14 13:45:07 +0530

    Create Defaultconstructor.java

---
+++ Defaultconstructor.java
@@ -0,0 +1,17 @@
+package oops;
+class DefaultConst
+{
+	DefaultConst()
+	{
+		System.out.println("This is default constructor");
+	}
+	
+	}
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		DefaultConst dc=new DefaultConst();
+
+	}
+
+}
